0170fa740044f4df622631afdbf7340633aaaa5b
numme <nummer14@naver.com>
2025-06-25 09:10:27 +0900

    Fifth Commit

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,25 @@
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.reflect.Method;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD})
-@interface CustomInfo {
-    String author();
-    String date();
-    int version() default 1;
-}
-
-@CustomInfo(author = "h662", date = "2025-06-24", version = 2)
-class Demo {
-
-    @CustomInfo(author = "h663", date ="2026-06-26")
-    public void display() {
-        System.out.println("Display method executed.");
-    }
-}
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Demo demo = new Demo();
-        Class<?> demoClass = demo.getClass();
-
-        if (demoClass.isAnnotationPresent(CustomInfo.class)) {
-            CustomInfo classInfo = demoClass.getAnnotation(CustomInfo.class);
-            System.out.println("Author: " + classInfo.author());
-            System.out.println("Date: " + classInfo.date());
-            System.out.println("Version: " + classInfo.version());
-        }
+    public static void writeFile(String filename,String content) {
+        FileWriter writer = null;
 
         try {
-            Method m = demoClass.getMethod("display");
-
-            if(m.isAnnotationPresent(CustomInfo.class)) {
-                CustomInfo mi = m.getAnnotation(CustomInfo.class);
-
-                System.out.println("Author: " + mi.author());
-                System.out.println("Date: " + mi.date());
-                System.out.println("Version: " + mi.version());
+            writer = new FileWriter(filename);
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch(NoSuchMethodException e) {
-            e.getStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
