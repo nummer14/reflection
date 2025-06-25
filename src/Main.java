87d5730c6d96b58a0af4a781872f6659d495acab
numme <nummer14@naver.com>
2025-06-25 09:14:19 +0900

    Eighth Commit

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,28 @@
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class Main {
-    public static void writeFile(String filename,String content) {
-        FileWriter writer = null;
+    public static void writeFile(String filename, String content) {
+        Path filePath = Paths.get(filename);
 
-        try {
-            writer = new FileWriter(filename);
-            writer.write(content);
+        try (FileChannel writeChannel = FileChannel.open(filePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            buffer.put(content.getBytes());
+            buffer.flip();
+            writeChannel.write(buffer);
+            System.out.println("파일을 성공적으로 작성 완료했습니다.");
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (writer != null) writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
     public static void main(String[] args) {
-        writeFile("lunch-menu.txt","오늘의 점심 메뉴는 무엇인가요?");
-        System.out.println("파일 생성 완료");
+        writeFile("dinner-menu.txt", "오늘의 저녁 메뉴는 뭘까요?");
     }
 }
