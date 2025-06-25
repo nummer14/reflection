c13887620c6887bf9a00ebaaad9f0952c48e4ded
numme <nummer14@naver.com>
2025-06-25 09:15:26 +0900

    Ninth Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
@@ -22,7 +23,29 @@ public class Main {
         }
     }
 
+    public static void readFile(String filename) {
+        Path filePath = Paths.get(filename);
+
+        try (FileChannel readChannel = FileChannel.open(filePath, StandardOpenOption.READ)) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            int bytesRead = readChannel.read(buffer);
+
+            while (bytesRead != -1) {
+                buffer.flip();
+
+                while (buffer.hasRemaining()) {
+                    System.out.println((char) buffer.get());
+                }
+                buffer.clear();
+                bytesRead = readChannel.read(buffer);
+            }
+            System.out.println("\n파일 읽기가 완료 되었습니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        writeFile("dinner-menu.txt", "오늘의 저녁 메뉴는 뭘까요?");
+        readFile("dinner-menu.txt");
     }
 }
