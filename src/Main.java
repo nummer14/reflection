87ac810037aa553c2520cc275c26c38e06ade029
numme <nummer14@naver.com>
2025-06-25 09:08:32 +0900

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,26 @@
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-class ReflectionDemo {
-    public  String noSecret = "안비밀입니다.";
-    private String secret = "비밀입니다.";
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@interface CustomInfo {
+    String author();
+    String date();
+    int version() default 1;
+}
 
-    public ReflectionDemo() {
-        System.out.println("ReflectionDemo 생성자 실행");
-    }
+@CustomInfo(author = "h662", date = "2025-06-24", version = 2)
+class Demo {
 
-    public String greet(String name) {
-        return "Hello, " + name;
-    }
-
-    private String reveal(String code) {
-        return "Access granted to: " + code;
+    @CustomInfo(author = "h663", date ="2026-06-26")
+    public void display() {
+        System.out.println("Display method executed.");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Class<?> clazz = ReflectionDemo.class;
-
-        System.out.println("클래스 이름: " + clazz.getName());
-
-        System.out.println("\n[필드 목록]");
-        Field[] fields = clazz.getDeclaredFields();
-        for(Field field: fields) {
-            System.out.println("필드: " + field.getName());
-        }
-
-        System.out.println("\n[메서드 목록]");
-        Method[] methods = clazz.getDeclaredMethods();
-        for(Method method: methods) {
-            System.out.print("메서드: " + method.getName());
-            for(Class<?> paramType: method.getParameterTypes()) {
-                System.out.println(" 파라미터 타입: " + paramType.getSimpleName());
-            }
-        }
-
-        try {
-            Object instance = clazz.getDeclaredConstructor().newInstance();
-
-            Method greetMethod = clazz.getMethod("greet", String.class);
-            Object greetResult = greetMethod.invoke(instance, "h662");
-            System.out.println("\n [퍼블릭 메서드 실행 결과]");
-            System.out.println("greet(): " + greetResult);
-
-            Method revealMethod = clazz.getDeclaredMethod("reveal", String.class);
-            revealMethod.setAccessible(true);
-            Object revealResult = revealMethod.invoke(instance, "abcd");
-            System.out.println("\n [프라이빗 메서드 실행 결과]");
-            System.out.println("reveal(): " + revealResult);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
